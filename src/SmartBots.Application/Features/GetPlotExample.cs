a0a12c6823467cd93dc5d8da9b0bd5da50da4936
Bashir Manafikhi <bashir.manafikhi@gmail.com>
2024-10-14 21:31:36 +0300

    add plot example

---
+++ src/SmartBots.Application/Features/GetPlotExample.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using ScottPlot;
+using SmartBots.Application.Features.Todos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartBots.Application.Features
+{
+    public class GetPlotExample : IRequest<Plot>
+    {
+
+    }
+    public class GetPlotExampleHandler : IRequestHandler<GetPlotExample, Plot>
+    {
+        public Task<Plot> Handle(GetPlotExample request, CancellationToken cancellationToken)
+        {
+            Plot myPlot = new();
+
+            double[] sin = Generate.Sin(51);
+            double[] cos = Generate.Cos(51);
+            myPlot.Add.Signal(sin);
+            myPlot.Add.Signal(cos);
+
+            return Task.FromResult(myPlot);
+        }
+    }
+}
--- src/SmartBots.Client/Program.cs
+++ src/SmartBots.Client/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SmartBots.Client;
+using SmartBots.Application;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddCascadingAuthenticationState();
+
+builder.Services.AddApplicationLayer();
 builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
 
 await builder.Build().RunAsync();
